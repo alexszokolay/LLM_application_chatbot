d8864a04f7d900726ddda403a6a0acc7625bb225
Alex Szokolay <90335498+alexszokolay@users.noreply.github.com>
2025-09-10 18:08:56 -0400

    implement chatbot + chatbot webpage

--- static/script.js
+++ static/script.js
@@ -42,7 +42,7 @@ const sendMessage = async (message) => {
   messagesContainer.appendChild(loadingtextElement);
 
   async function makePostRequest(msg) {
-    const url = 'www.example.com';  // Make a POST request to this url
+    const url = '/chatbot';
     const requestBody = {
       prompt: msg
     };
